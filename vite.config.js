bdc29032e2af134f82826d2131db0153bc08257e
David-Kasilia <davidkasilia001@gmail.com>
2023-06-11 22:59:20 +0300

    Coaching strategies module added

--- vite.config.js
+++ vite.config.js
@@ -3,73 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 import { generateSW } from "workbox-build";
 
+import { cachingStrategies } from "./cachingStrategies.js";
+
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "none",
       workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "images",
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
-              },
-            },
-          },
-          {
-            urlPattern: /\.(?:js|css)$/,
-            handler: "StaleWhileRevalidate",
-            options: {
-              cacheName: "assets",
-              expiration: {
-                maxAgeSeconds: 24 * 60 * 60, // 1 day
-              },
-            },
-          },
-          {
-            urlPattern: /\.(?:woff|woff2|ttf|otf)$/,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "fonts",
-              expiration: {
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/api.example.com/,
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "api",
-              expiration: {
-                maxAgeSeconds: 60 * 60, // 1 hour
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/cdn.example.com/,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "cdn",
-              expiration: {
-                maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
-              },
-            },
-          },
-          {
-            urlPattern: /\/api\/data/,
-            handler: "NetworkOnly",
-            options: {
-              cacheName: "dynamic-api",
-            },
-          },
-          // Add more caching strategies as needed
-        ],
+        runtimeCaching: [...cachingStrategies],
       },
       injectRegister: false,
       injectManifest: false,
